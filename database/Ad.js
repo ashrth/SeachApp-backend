48451d9618b44c56298abc1ed282bec0fa63855f
ashrth <aashrrita@gmail.com>
2022-10-10 02:13:25 +0530

    first commit

---
+++ database/Ad.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+const adSch = new mongoose.Schema({
+  name: String,
+  headline: String,
+});
+
+module.exports = mongoose.model("Ads", adSch);
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("./database/config");
+const Ad = require("./database/Ad");
+
+const app = express();
+app.use(express.json());
+
+app.get("/:key", async (req, resp) => {
+  let ads = await Ad.find({
+    "$or": [
+      {
+          name: { $regex: req.params.key }
+      },
+      {
+          company: { $regex: req.params.key }
+      },
+      {
+          category: { $regex: req.params.key }
+      }
+  ]
+          
+      
+      }
+  );
+  if (ads.length > 0) {
+    resp.send(ads);
+  } else {
+    resp.send("No Product Found");
+  }
+});
+
+app.listen(3000);
+
+
