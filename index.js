826cb3880ba39a14f10de115a7dfe54e4a4d7b49
ashrth <aashrrita@gmail.com>
2022-10-11 11:25:52 +0530

    init

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/ads", async (req, resp) => {
 });
 
 app.get("/ads/:key", async (req, resp) => {
-  let ads = await Ad.findOne({
+  let ads = await Ad.find({
     "$or": [
   {
       name: { $regex: req.params.key }
